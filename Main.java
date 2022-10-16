00188edaf4ea8b4b1bbc11af4ede39eae1d6c31f
Magno Junior <magnobrito00@gmail.com>
2022-10-16 02:41:40 -0300

    Making the final steps

--- Main.java
+++ Main.java
@@ -3,54 +3,54 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Scanner inner = new Scanner(System.in);
-		System.out.println("Welcome to my own source code for binary search");
-		ArrayList<Integer> list_of_numbers = new ArrayList<Integer>();
-		while(true) {
-			System.out.println("If ADD type 1 or 2 to EXIT");
-			int verification = inner.nextInt();
-			if( verification == 1) {
-				System.out.println("Type a number:");
-				int number = inner.nextInt();
-				list_of_numbers.add(number);
-			}else if(verification == 2) {
-				break;
-			}else {
-				System.out.println("Incorrect input");
-			}	
-		}
+		System.out.println("Welcome to my own source code for binary search. \n");
+		System.out.println("Type the size of your list:\n");
 		
+		int list_size = inner.nextInt();
+		int [] list_of_numbers = new int[list_size];
+		
+		for(int i = 0; i < list_size; i++) {
+			System.out.println("Type the number: ");
+			int number = inner.nextInt();
+			list_of_numbers[i] = number;
+			
+		}
 		System.out.println("Raw list\n");
-		for(int i = 0; i < list_of_numbers.size(); i++) {
-			System.out.print(list_of_numbers.get(i) + " ");
+		for(int i = 0; i < list_of_numbers.length; i++) {
+			System.out.print(list_of_numbers[i] + " ");
 		}
 		
 		System.out.println("\nSorted list\n");
-		list_of_numbers.sort(null);
-		for(int i = 0; i < list_of_numbers.size(); i++) {
-			System.out.print(list_of_numbers.get(i) + " ");
+		
+		for(int i = 0; i < list_of_numbers.length; i++) {
+			System.out.print(list_of_numbers[i] + " ");
 		}
+		
+		int answer  = binary_search(1,list_of_numbers);
+		System.out.println(answer);
 	}
 	
 
 	
-	public static int binary_search(int value, ArrayList<Integer> array_enter) {
+	public static int binary_search(int value, int [] array_enter) {
 		
-		ArrayList<Integer> new_array = new ArrayList<Integer>();
-		new_array = array_enter;
+		int[] new_array = new int[array_enter.length];
+		new_array = array_enter.clone();
 		int begin = 0;
-		int end = array_enter.size()-1;
+		int end = array_enter.length;
 		
 		while( end >= begin) {
-			if( new_array.size() == 0)return -1;
+			if( new_array.length == 0)return -1;
 			else {
 				int middle = (begin + end)/2;
-				if( new_array.get(middle) == value) {
+				if( new_array[middle] == value) {
 					return middle;
-				}else if(new_array.get(middle) > value) {
-					new_array = (ArrayList<Integer>) array_enter.subList(0, middle);
+				}else if(new_array[middle] > value) {
+					System.arraycopy(array_enter, 0, new_array, 0,middle-1);
 					binary_search(value, new_array );
-				}else if(new_array.get(middle) < value) {
-					new_array = (ArrayList<Integer>) array_enter.subList(middle + 1, new_array.size());
+				}else if(new_array[middle] < value) {
+					System.arraycopy(array_enter, 0, new_array, 0,middle+1);
+					binary_search(value, new_array );
 				}
 				return -1;
 			}
